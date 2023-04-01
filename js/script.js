a72ca26e620886e519e79833f5726b99d7a7700b
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-04-02 01:41:24 +0300

    Continuation of better layout

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const prompt = document.querySelector("box");
+function adnan() {
+    onClick = document.write('I am a Man!');
+}
+
+function legend(){
+    document.getElementById('box').innerHTML = string;    
+}
+
+
+
