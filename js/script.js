f180ceda7f0eba7b8b7fdba16129efb236f7cfe1
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-05-23 12:47:04 +0300

    contact page update

--- js/script.js
+++ js/script.js
@@ -10,6 +10,15 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-} 
+  setTimeout(showSlides, 1000); 
+};
 
+
+var mainButton = document.querySelector("#main_btn");
+var resumePreview = document.getElementByClassName("resume");
+
+mainButton.addEventListener("click", showResume);
+
+function showResume(){
+  alert("This is my resume");
+}
